9849423889c3f8f0fd65a77e25335ab4b5caa007
Mathilde De Oliveira <matde-ol@2A6.42angouleme.fr>
2023-11-22 13:32:25 +0100

    try

--- get_next_line.h
+++ get_next_line.h
@@ -25,6 +25,6 @@ char	*ft_substr(char *src, int start, int len);
 char	*ft_strlcpy_f(char *src, int len);
 int		ft_strchr(char *src);
 int		ft_strlen(char *src);
-char	*ft_strlcpy_f(char *src, int len);
+int		ft_security(char *reste, char *dest);
 char	*get_next_line(int fd);
 #endif
